33269a2cb75e5beaafcc50b0aafb1e7f47da3a02
aaditg01 <67324673+aaditg01@users.noreply.github.com>
2021-10-24 17:55:20 +0530

    Add files via upload

---
+++ Mario-Level-3-master/sketch.js
@@ -0,0 +1,103 @@
+let mario, mario_running, mario_collided;
+let bg, bgImage;
+let brickGroup, brickImage;
+let coinGroup, coinImage, coinSound;
+let score=0;
+
+function preload(){
+  mario_running =  loadAnimation("images/mar1.png","images/mar2.png","images/mar3.png",
+  "images/mar4.png","images/mar5.png","images/mar6.png","images/mar7.png");
+  bgImage = loadImage("images/bgnew.jpg");
+  brickImage = loadImage("images/brick.png");
+  coinSound= loadSound("./sounds/coinSound.mp3");
+  coinImage = loadAnimation("./images/con1.png","./images/con2.png","./images/con3.png","./images/con4.png","./images/con5.png","./images/con6.png");
+}
+
+function setup() {
+  createCanvas(1000, 600);
+
+  bg = createSprite(580,300);
+  bg.addImage(bgImage);
+  bg.scale =0.5;
+ 
+  bg.velocityX = -6;
+  mario = createSprite(200,505,20,50);
+  mario.addAnimation("running", mario_running);
+  mario.scale =0.3;
+  ground = createSprite(200,585,400,10);
+  ground.visible = false;
+  bricksGroup = new Group();
+  coinGroup= new Group();
+}
+
+function draw() {
+ 
+  if (bg.x < 100){
+    bg.x=bg.width/4;
+  }
+  if(mario.x<200){
+    mario.x=200;
+  }
+
+  if(mario.y<50){
+    mario.y=50;
+  }
+
+  if(keyDown("space") ) {
+    mario.velocityY = -16;
+  }
+  mario.velocityY = mario.velocityY + 0.5;
+  generateBricks();
+  for(var i = 0 ; i< (bricksGroup).length ;i++){
+    var temp = (bricksGroup).get(i) ;
+    
+    if (temp.isTouching(mario)) {
+      mario.collide(temp);
+    }
+    
+  }
+  generateCoins();
+
+  for(let i=0; i<coinGroup.length; i++){
+    let temp = (coinGroup).get(i) ;
+    console.log(temp);
+    if (temp.isTouching(mario)){
+      score++;
+      coinSound.play();
+      temp.destroy();
+      temp = null;
+    }
+  }
+
+  mario.collide(ground);
+  drawSprites();
+  textSize(30);
+  fill("black");  
+  text("Coins collected : " + score , 500,50)
+}
+
+
+function generateBricks() {
+  if (frameCount % 70 === 0) {
+    var brick = createSprite(1200,120,40,10);
+    brick.y = random(50,450);
+    brick.addImage(brickImage);
+    brick.scale = 0.5;
+    brick.velocityX = -5;
+    
+    brick.lifetime =250;
+    bricksGroup.add(brick);
+  }
+}
+
+
+function generateCoins() {
+  if(frameCount % 20 === 0){
+    let coin = createSprite(1200,random(80,250),40,10);
+    coin.addAnimation("coin" , coinImage);
+    coin.scale=0.1;
+    coin.velocityX= -10;
+    coin.lifetime = 1200;
+    coinGroup.add(coin);
+  }
+}
